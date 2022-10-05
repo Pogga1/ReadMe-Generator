0a300f68acc56f242b27d789b0b4011c382a0e98
Pogga1 <Andreweysoldt@gmail.com>
2022-10-05 15:58:03 -0700

    finished the program and now a professional ReadMe can be generated off of user input

---
+++ README.md
@@ -0,0 +1,31 @@
+# ReadMe-Generator
+  ## https://github.com/Pogga1/ReadMe-Generator
+  # Description
+  
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+# License
+[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+* [Contributing](#contributions)
+* [Tests](#test)
+* [Questions](#questions)
+# Installation
+The following necessary dependencies must be installed to run the application: 
+The following necessary dependencies must be installed to run the application: node.js inquirer and git bash or the terminal in mac
+# Usage
+  In order to use this app, In order to use this app, the user would follow the prompts in the terminal and answer the questions via an input field or choice field depending on the question. This would then create a README file that the user could navigate to and use.
+  # License
+[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  # Contributions
+  Contributors: Andrew Eysoldt
+  # Tests
+  The following is needed to run tests: N/A
+  # Questions
+  If you have any questions about the repository contact Andrew Eysoldt directly at : Andreweysoldt@gmai.com.
+
+
+
+  Have a nice day!!!
+  
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -4,58 +4,83 @@ const renderLicenseBadge = (data) => {
   let licenseBadge = "";
 
   if (data.LicenseType === "MIT") {
-    licenseBadge = '[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    licenseBadge =
+      "[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
     return licenseBadge;
   }
   if (data.LicenseType === "Apache") {
     licenseBadge =
       "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-      return licenseBadge;
+    return licenseBadge;
   }
   if (data.LicenseType === "Boost") {
     licenseBadge =
       "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
-      return licenseBadge;
+    return licenseBadge;
   }
   if (data.LicenseType === "BDS 3-Clause") {
     licenseBadge =
       "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
-      return licenseBadge;
-  } if (data.License === false) {
     return licenseBadge;
   }
-
+  if (data.License === false) {
+    return licenseBadge;
+  }
 };
-
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 const renderLicenseTableOfContents = (data) => {
-  if(data.License === true) {
-    return `* [License](#license)`
+  if (data.License === true) {
+    return `* [License](#license)`;
   } else {
-    return ''
+    return "";
   }
-}
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 const renderLicenseSection = (data) => {
-  if (data.License === false){
-    return ''
-  } else{ 
+  if (data.License === false) {
+    return "";
+  } else {
     return `# License
-${renderLicenseBadge(data)}`
+${renderLicenseBadge(data)}`;
   }
-}
+};
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
   return `# ${data.Title}
+  ## https://github.com/${data.Github}/${data.Title}
+  ## https://drive.google.com/file/d/1KDi9jSeM7r12caHWClxOP08dDziXMlPG/view
+  # Description
+  ${data.Description}
 # Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
 ${renderLicenseTableOfContents(data)}
 ${renderLicenseSection(data)}
-`;
+* [Contributing](#contributions)
+* [Tests](#test)
+* [Questions](#questions)
+# Installation
+The following necessary dependencies must be installed to run the application: 
+${
+  data.Installation
 }
+# Usage
+  In order to use this app, ${data.Usage}
+  ${ renderLicenseSection(data)}
+  # Contributions
+  Contributors: ${data.Contributions}
+  # Tests
+  The following is needed to run tests: ${data.Tests}
+  # Questions
+  If you have any questions about the repository contact ${
+    data.Contributions
+  } directly at : ${data.Email}.
+  `;
+};
 
 module.exports = generateMarkdown;
